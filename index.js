275a90fcc13386229e746daa45bc61623a35f37d
yani82 <yani.iman1@gmail.com>
2021-02-09 02:05:41 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr) {
+  const time = parseInt(timeStr, 10);
+  if (time < 12) return "Good Morning"
+  if (time > 17) return "Good Evening" 
+  return "Good Afternoon" 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg; 
+}
